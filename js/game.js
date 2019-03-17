4adb8bab8911f71965a103b22d9f27d8f83f8b1b
Hongjun He <anthonyho02015@gmail.com>
2019-03-17 06:40:01 +0000

    added endings

--- js/game.js
+++ js/game.js
@@ -2,14 +2,14 @@
 
 var game = {
     music: "Untitled.mp3",
-    background_image: "wakeF150.gif",
     levels: {
         start: {
+            background_image: "wakeF150.gif",
             message: ["You woke up, you have work today.","Will you go to work?"],
             choices: [
                 {
                     text: "Go work",
-                    nextLevel: "seeButterFly",
+                    nextLevel: "busStop",
                 },
 
                 {
@@ -20,21 +20,6 @@ var game = {
             ]
         },
 
-        seeButterFly: {
-            background_image: "toWorkF150.gif",
-            message: ["You come across a butterfly."],
-            choices: [
-                {
-                    text: "touch",
-                    nextLevel: "earthQuake",
-                },
-                
-                {
-                    text: "ignore",
-                    nextLevel: "arrive",
-                },
-            ]
-        },
 
         phoneCall: {
             background_image: "phone150F.gif",
@@ -42,7 +27,7 @@ var game = {
             choices: [
                 {
                     text: "go to work",
-                    nextLevel: "seeButterFly",
+                    nextLevel: "busStop",
                 },
                 
                 {
@@ -52,10 +37,10 @@ var game = {
             ]
         },
         
-        //bad ending 1
+        //bad endings
         earthQuake: {
             background_image: "earthQuakeF80.gif",
-            message: ["!?","there's an earthquake!","you're dead dead."],
+            message: ["!?","there's an earthquake!","you're dead."],
             choices: [
                 {
                     text: "Start over",
@@ -63,22 +48,214 @@ var game = {
                 },
             ]
         },
-        //-------
-        arrive: {
-            background_image: "",
-            message: ["you arrive at work place",""],
+        
+        hit: {
+            background_image: "endingHit.gif",
+            message: ["You died."],
             choices: [
                 {
-                    text: "Start over",
+                    text: "restart",
+                    nextLevel: "start",
+                },
+                
+            ]
+        },
+        
+        insane: {
+            background_image: "insane.gif",
+            message: ["You went to the top of the office building.","you see the monster.","your brain can not comprehend what is happening.","you went insane as a result."],
+            choices: [
+                {
+                    text: "restart",
                     nextLevel: "start",
                 },
             ]
         },
-         
+        //related to travel to work
+        seeButterFly: {
+            background_image: "toWorkF150.gif",
+            message: ["You come across a butterfly."],
+            choices: [
+                {
+                    text: "touch",
+                    nextLevel: "city",
+                },
+                
+                {
+                    text: "ignore",
+                    nextLevel: "late",
+                },
+            ]
+        },
+        
+        onTime: {
+            background_image: "onTimeF150.gif",
+            message: ["bus came much earlier than expected.","you arrive at work place on time."],
+            choices: [
+                {
+                    text: "continue.",
+                    nextLevel: "fakeEQ",
+                },
+            ]
+        },
+        
+        late: {
+            background_image: "late.gif",
+            message: ["you arrive at work place late."],
+            choices: [
+                {
+                    text: "continue.",
+                    nextLevel: "city",
+                },
+            ]
+        },
+        
+        busStop: {
+            background_image: "busStopF150.gif",
+            message: ["you arrive at bus stop.","bus is coming in a hour"],
+            choices: [
+                {
+                    text: "talk to people",
+                    nextLevel: "talk",
+                },
+                
+                 {
+                    text: "wait for bus",
+                    nextLevel: "onTime",
+                },
+                
+                 {
+                    text: "walk to work",
+                    nextLevel: "seeButterFly",
+                },
+            ]
+        },
+        
+        talk: {
+            background_image: "talk.gif",
+            message: ["a weird guy is talking.","'there will be a catastrophe!'","you think he is crazy."],
+            choices: [
+                {
+                    text: "continue.",
+                    nextLevel: "busStop",
+                },
+            ]
+        },
+        // city 
+        city: {
+            background_image: "city.png",
+            message: ["this is the city.","!?"],
+            choices: [
+                {
+                    text: "continue.",
+                    nextLevel: "cityRuin",
+                },
+            ]
+        },
+        
+        cityRuin: {
+            background_image: "cityRuin.png",
+            message: ["the city is in ruins."],
+            choices: [
+                {
+                    text: "continue.",
+                    nextLevel: "monster",
+                },
+            ]
+        },
+            // monster
+        monster: {
+            background_image: "cityMonsterF150.gif",
+            message: ["a thing emerged from the depth of the ruin."],
+            choices: [
+                {
+                    text: "continue.",
+                    nextLevel: "decide", //<------------------------------------
+                },
+            ]
+        },
+        
+        //in side decides
+        fakeEQ: {
+            background_image: "earthQuakeF80.gif",
+            message: ["There is an earthQuake!","you must get out as soon as it stops."],
+            choices: [
+                {
+                    text: "ok",
+                    nextLevel: "inside",
+                },
+                
+            ]
+        },
+        
+        inside: {
+            background_image: "esscapeF3.png",
+            message: ["you must run."],
+            choices: [
+                {
+                    text: "run to roof top.",
+                    nextLevel: "insane",
+                },
+                
+                 {
+                    text: "run to F2.",
+                    nextLevel: "F2",
+                },
+                
+                 {
+                    text: "run outside.",
+                    nextLevel: "city",
+                },
+            ]
+        },
+        
+        F2: {
+            background_image: "f2Blocked.gif",
+            message: ["F2 is blocked."],
+            choices: [
+                {
+                    text: "run up",
+                    nextLevel: "inside",
+                },
+                
+                {
+                    text: "run down",
+                    nextLevel: "decide",
+                }
+            ]
+        },
         
         
+        //outside decides
+        
+         decide: {
+            background_image: "decide.png",
+            message: ["you look at the monster.","which way will you run?"],
+            choices: [
+                {
+                    text: "left",
+                    nextLevel: "goodEnd",
+                },
+                
+                {
+                    text: "right",
+                    nextLevel: "hit",
+                }
+            ]
+        },
         
         
-        
+        //live
+        goodEnd: {
+            background_image: "survive.png",
+            message: ["you ran to the evacuation zone.","you have been transported out of the city.","you live to work another day."],
+            choices: [
+                {
+                    text: "ok",
+                    nextLevel: "start",
+                },
+                
+            ]
+        },
     }
 };
